065c7368441a4f33fc0ab7953ffab387632b28f6
meterslug2 <meterslug2@gmail.com>
2024-09-25 14:14:34 +0900

    point cloud 추가

--- ARIOS/Assets/GetCameraFrame/GetCameraFrame.cs
+++ ARIOS/Assets/GetCameraFrame/GetCameraFrame.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using TMPro;
 using Unity.Collections;
 using Unity.Collections.LowLevel.Unsafe;
@@ -6,6 +7,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.XR.ARFoundation;
 using UnityEngine.XR.ARSubsystems;
+using System.IO;
 
 public class GetCameraFrame : MonoBehaviour
 {
@@ -13,15 +15,26 @@ public class GetCameraFrame : MonoBehaviour
     public RawImage camFrame;//실시간 카메라 프레임
     public RawImage captureImg;//캡쳐된 프레임 포인트 클라우드 color를 위한 캡쳐이미지
     public ARCameraManager cameraManager;
+    public ARPointCloudManager pcdManager;
     public TMP_Text imgSizeTxt;
     public TMP_Text errorTxt;
+    public TMP_Text saveTxt;
     protected Texture2D cameraTexture;
     protected int screenW;
     protected int screenH;
     string info = string.Empty;
-    public Button captureBtn;
-    public Canvas uiCanvas;
+
+    //로그 체크용으로만 사용하는변수 
     bool first = true;
+    bool firstPcd = true;
+
+    bool isProcess = false;
+    protected List<Vector3> pcdList = new List<Vector3>();
+    protected List<Vector3> tempPcdList = new List<Vector3>();
+    protected List<Vector3> cameraPathList = new List<Vector3>();
+    protected List<Color> colorList = new List<Color>();
+    protected int saveCnt = 0;
+    protected int maxCnt = 200000;
     //아이패드 screen 해상도 2388, 1688
     void Start()
     {
@@ -34,90 +47,209 @@ public class GetCameraFrame : MonoBehaviour
             cameraManager = GetComponent<ARCameraManager>();
         }
         CheckScreenSize();
-        CanvasScaler cs = uiCanvas.GetComponent<CanvasScaler>();
-        //cs.uiScaleMode = ScaleMode.
         cameraManager.frameReceived += OnReceivedCameraFrame;
-        captureBtn.onClick.AddListener(OnClickCaptureFrame);
+        pcdManager.pointCloudsChanged += OnPointCloudChanged;
+    }
+    private void Update()
+    {
+        CurrentSaveState();
     }
     unsafe void OnReceivedCameraFrame(ARCameraFrameEventArgs args)
     {
-        try
+        if (isProcess == false)
         {
-            if (cameraManager != null)
+            isProcess = true;
+            try
             {
-                if (!cameraManager.TryAcquireLatestCpuImage(out XRCpuImage image))
+                if (cameraManager != null)
                 {
-                    return;
+                    if (!cameraManager.TryAcquireLatestCpuImage(out XRCpuImage image))
+                    {
+                        return;
+                    }
+                    if (first)
+                    {
+                        first = false;
+                        info += string.Format("imgW:{0} imgH:{1}\n", image.width, image.height);
+                    }
+                    imgSizeTxt.text = info;
+
+                    var conversionParams = new XRCpuImage.ConversionParams
+                    {
+                        inputRect = new RectInt(0, 0, image.width, image.height),
+                        outputDimensions = new Vector2Int(image.width, image.height),
+                        outputFormat = TextureFormat.RGB24,
+                        transformation = XRCpuImage.Transformation.MirrorX
+                    };
+
+
+                    int size = image.GetConvertedDataSize(conversionParams);
+                    var buffer = new NativeArray<byte>(size, Allocator.Temp);
+                    image.Convert(conversionParams, new IntPtr(buffer.GetUnsafePtr()), buffer.Length);
+
+                    captureImg.rectTransform.sizeDelta =
+                        new Vector2(conversionParams.outputDimensions.x, conversionParams.outputDimensions.y);
+                    captureImg.rectTransform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
+                    captureImg.texture = cameraTexture;
+                    image.Dispose();
+
+                    if (cameraTexture == null)
+                    {
+                        cameraTexture = new Texture2D
+                                (
+                                conversionParams.outputDimensions.x,
+                                conversionParams.outputDimensions.y,
+                                conversionParams.outputFormat,
+                                false
+                                );
+                    }
+                    cameraTexture.LoadRawTextureData(buffer);
+                    cameraTexture.Apply();
+                    buffer.Dispose();
                 }
-                if (first)
-                {
-                    first = false;
-                    info += string.Format("imgW:{0} imgH:{1}", image.width, image.height);
-                }
-                imgSizeTxt.text = info;
-
-                var conversionParams = new XRCpuImage.ConversionParams
-                {
-                    inputRect = new RectInt(0, 0, image.width, image.height),
-                    outputDimensions = new Vector2Int(image.width / 2, image.height / 2),
-                    outputFormat = TextureFormat.RGB24,
-                    transformation = XRCpuImage.Transformation.None
-                };
-
-
-                int size = image.GetConvertedDataSize(conversionParams);
-                var buffer = new NativeArray<byte>(size, Allocator.Temp);
-                image.Convert(conversionParams, new IntPtr(buffer.GetUnsafePtr()), buffer.Length);
-                //var rawTextureData = cameraTexture.GetRawTextureData<byte>();
-                //image.Convert(conversionParams, new NativeArray<byte>(rawTextureData, Allocator.Temp));
-                
-                captureImg.rectTransform.sizeDelta =
-                    new Vector2(conversionParams.outputDimensions.x, conversionParams.outputDimensions.y);
-                captureImg.rectTransform.localScale = new Vector3(0.3f, 0.3f, 0.3f);
-                captureImg.texture = cameraTexture;
-                image.Dispose();
-
-                if (cameraTexture == null)
-                {
-                    cameraTexture = new Texture2D
-                            (
-                            conversionParams.outputDimensions.x,
-                            conversionParams.outputDimensions.y,
-                            conversionParams.outputFormat,
-                            false
-                            );
-                }
-                cameraTexture.LoadRawTextureData(buffer);
-                cameraTexture.Apply();
-                buffer.Dispose();
+            }
+            catch (System.Exception e)
+            {
+                errorTxt.text = e.Message;
             }
         }
-        catch (System.Exception e)
-        {
-            errorTxt.text = e.Message;
-        }
     }
-    protected void OnClickCaptureFrame()
+    protected void OnPointCloudChanged(ARPointCloudChangedEventArgs args)
     {
-    }
-    protected void ChangePixelColor(Texture2D texture)
-    {
-        if (texture == null)
+        if(firstPcd)
         {
-            return;
+            firstPcd = false;
+            info += string.Format("point cloud get");
         }
-        for (int x = 0; x < 100; x++)
+        if(isProcess)
         {
-            for (int y = 0; y < 100; y++)
+            foreach(ARPointCloud pointCloud in args.added)
             {
-                texture.SetPixel(x, y, Color.black);
+                if(pointCloud.positions.HasValue)
+                {
+                    pcdList.AddRange(pointCloud.positions.Value);
+                    tempPcdList.Clear();
+                    tempPcdList.AddRange(pointCloud.positions.Value);
+                    cameraPathList.Add(arCamera.transform.position);
+                    
+                   for(int i=0; i<tempPcdList.Count;i++)
+                    {
+                        Vector3 screenPos = arCamera.WorldToScreenPoint(tempPcdList[i]);
+                        int x = (int)screenPos.x;
+                        int y = (int)screenPos.y;
+                        if(cameraTexture == null)
+                        {
+                            Color pcdColor = Color.black;
+                            colorList.Add(pcdColor);
+                        }
+                        else
+                        {
+                            if (x >= 0 && x<cameraTexture.width
+                                &&  y>=0 && y<cameraTexture.height)
+                            {
+                                Color pcdColor = cameraTexture.GetPixel(x, y);
+                                colorList.Add(pcdColor);
+                            }
+                            else
+                            {
+                                Color pcdColor = Color.black;
+                                colorList.Add(pcdColor);
+                            }
+                        }
+                    }
+                }
             }
+            foreach(ARPointCloud pointCloud in args.updated)
+            {
+                if(pointCloud.positions.HasValue)
+                {
+                    pcdList.AddRange(pointCloud.positions.Value);
+                    tempPcdList.Clear();
+                    tempPcdList.AddRange(pointCloud.positions.Value);
+                    cameraPathList.Add(arCamera.transform.position);
+                    for(int i=0; i<tempPcdList.Count;i++)
+                    {
+                        Vector3 screenPos = arCamera.WorldToScreenPoint(tempPcdList[i]);
+                        int x = (int)screenPos.x;
+                        int y = (int)screenPos.y;
+                        if(cameraTexture == null)
+                        {
+                            Color pcdColor = Color.black;
+                            colorList.Add(pcdColor);
+                        }
+                        else
+                        {
+                            if(x >=0 && x<cameraTexture.width &&
+                                y>=0 && y< cameraTexture.height)
+                            {
+                                Color pcdColor = cameraTexture.GetPixel(x, y);
+                                colorList.Add(pcdColor);
+                            }
+                            else
+                            {
+                                Color pcdColor = Color.black;
+                                colorList.Add(pcdColor);
+                            }
+                        }
+                    }
+                }
+            }
+            SavePCD();
         }
-        texture.Apply();
+        isProcess = false;
     }
-    protected void GetPixelColor(Texture2D targetTexture, int x, int y)
+    protected void SavePCD()
     {
+        if(pcdList.Count >= maxCnt)
+        {
+            //save
+            string pcdFileName = string.Format("PCDFile{0}.bin", saveCnt);
+            string camFileName = string.Format("CamPathFile{0}.bin", saveCnt);
+            string colorFileName = string.Format("colorFile{0}.bin", saveCnt);
 
+            string pcdPath = Path.Combine(Application.persistentDataPath, pcdFileName);
+            string camPath = Path.Combine(Application.persistentDataPath, camFileName);
+            string colorPath = Path.Combine(Application.persistentDataPath, colorFileName);
+
+            using (BinaryWriter writer = new BinaryWriter(File.Open(pcdPath, FileMode.Create)))
+            {
+                //writer.Write(pcdList.Count);
+                foreach(Vector3 point in pcdList)
+                {
+                    writer.Write(point.x);
+                    writer.Write(point.y);
+                    writer.Write(point.z);
+                }
+            }
+            using(BinaryWriter writer = new BinaryWriter(File.Open(camPath,FileMode.Create)))
+            {
+                //writer.Write(cameraPathList.Count);
+                foreach(Vector3 point in cameraPathList)
+                {
+                    writer.Write(point.x);
+                    writer.Write(point.y);
+                    writer.Write(point.z);
+                }
+            }
+            using (BinaryWriter writer = new BinaryWriter(File.Open(colorPath,FileMode.Create)))
+            {
+                foreach(Color color in colorList)
+                {
+                    writer.Write(color.r);
+                    writer.Write(color.g);
+                    writer.Write(color.b);
+                }
+            }
+            pcdList.Clear();
+            cameraPathList.Clear();
+            colorList.Clear();
+        }
+    }
+    protected void CurrentSaveState()
+    {
+        string s = string.Format("Current PCD Cnt : {0} saveFile Cnt : {1} color Cnt :{2} cameraPath Cnt{3}"
+            , pcdList.Count, saveCnt,colorList.Count,cameraPathList.Count);
+        saveTxt.text = s;
     }
     protected void CheckScreenSize()
     {
